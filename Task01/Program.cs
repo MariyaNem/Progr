d5062c7926cad8e48ab8ca757f64c78dad921e44
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-18 21:10:49 +0700

    solve task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите квадрат числа: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число: ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+if (numberB*numberB == numberA)
+{
+    Console.Write($"{numberA} является квадратом {numberB}");
+}
+else
+{
+    Console.Write($"{numberA} не является квадратом {numberB}");
+}
