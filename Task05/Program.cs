979e96e01de4adbbebb5e153d80ad02d8c55b369
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-19 20:14:46 +0700

    Solve task05

---
+++ Task05/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int negative = number*-1;
+while (number >= negative)
+{
+    Console.Write(negative + " ");
+    negative++;
+}
