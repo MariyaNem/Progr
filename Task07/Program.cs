b262dbd28cfd40bc5f0c43eb4daaefe3fc0b5040
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-19 23:50:51 +0700

    Solve task07

---
+++ Task07/Program.cs
@@ -0,0 +1,12 @@
+﻿//456 - 6
+//760 - 0
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>=100 && number<=999)
+{
+    Console.Write($"Последняя цифра в числе {number} - {number % 10}");
+}
+else
+{
+    Console.Write("Вы ввели не трёхзначное число.");
+}
