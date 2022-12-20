039384a24a3278f915586ad21fb833378132d53e
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-20 18:19:28 +0700

    Add and solve Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите первое число a=");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число b=");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write($"max={a}");
+}
+else
+{
+    Console.WriteLine($"max={b}");
+}
