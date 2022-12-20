b3615fccd856efc6ac7bdc8465897f71eba28c6a
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-20 18:31:18 +0700

    Add tack condition

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,4 +1,9 @@
-﻿Console.Write("Введите первое число a=");
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+//a = 5; b = 7 -> max = 7
+//a = 2 b = 10 -> max = 10
+//a = -9 b = -3 -> max = -3
+
+Console.Write("Введите первое число a=");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число b=");
 int b = Convert.ToInt32(Console.ReadLine());
