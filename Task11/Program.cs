517b7ff22309a47c769e3f4699d497c5f61f1166
Mariya Nemzorova <nemzorova777@mail.ru>
2022-12-22 19:59:30 +0700

    Add and solve Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая выводит случайное тёхзначное число и удаляет вторую цифру этого числа
+//456 -> 46
+//782 -> 72
+//918 -> 98
+
+int number = new Random().Next(100, 999);
+Console.WriteLine(number);
+int first = number / 100 * 10;
+int third = number % 10;
+int result = third + first;
+Console.WriteLine(result);
